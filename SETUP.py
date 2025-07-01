c544cefe5d957f8d4cd076520aaaaff0bbc3b480
Mirzakochak <117621343+Mirzakochak@users.noreply.github.com>
2025-07-01 14:08:20 +0330

    Add files via upload

--- SETUP.py
+++ SETUP.py
@@ -1,29 +1,31 @@
 import os
+
 required_packages = [
     "requests", 
     "pyfiglet", 
     "telebot", 
     "faker", 
-    "re", 
-    "json", 
-    "gates", 
-    "reg", 
     "beautifulsoup4", 
     "colorama",
     "pycountry",
     "cloudscraper",
     "user-agent"
 ]
+
 def upgrade_pip():
-    os.system("python.exe -m pip install --upgrade pip")
+    os.system("python3 -m pip install --upgrade pip")
+
 def install_packages(packages):
     for package in packages:
-        os.system(f"pip install {package}")
+        os.system(f"pip3 install {package}")
         print(f"DONE install [ {package} ]")
+
 def print_done_message():
     print("\nDone install :) ...... Mr.Ali")
+
 def print_done_message1():
-    input("\pass any key for exit :)")
+    input("\press any key to exit :)")
+
 if __name__ == "__main__":
     install_packages(required_packages)
     upgrade_pip()
